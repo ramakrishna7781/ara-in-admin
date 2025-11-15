bb00e9879c94d80e6d76031f88304dcf559a9496
Venkata Ramakrishna K <ramakrishna14636@gmail.com>
2025-11-16 01:15:39 +0530

    updated axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
-  // baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
