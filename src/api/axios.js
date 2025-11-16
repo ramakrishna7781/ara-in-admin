a76db53212a06813971fd694898047a3014dde6f
Venkata Ramakrishna K <ramakrishna14636@gmail.com>
2025-11-16 13:12:09 +0530

    done-final

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:8080",
-  // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: "https://shoppingapp-on6o.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: "https://shoppingapp-on6o.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
